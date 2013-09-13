0ce8b05db19d615290fa0d4266a6953d1f2c93ab
leeeunjae <scene978@gmail.com>
2013-09-14 03:30:12 +0900

    asd

--- public/javascripts/welcome.js
+++ public/javascripts/welcome.js
@@ -1,77 +1,77 @@
 $(function() {
-    getUserID();
-    getTeamList();
-    $('#btnLogout').click(btnLogoutAction);
+	getUserID();
+	getTeamList();
+	$('#btnLogout').click(btnLogoutAction);
 });
 
 var btnLogoutAction = function() {
-    $.ajax({
-        type:   'get',
-        url:    '/welcome/logout',
-        success: function(data) {
-            $(location).attr('href', '/');   
-        }
-    });
+	$.ajax({
+		type : 'get',
+		url : '/welcome/logout',
+		success : function(data) {
+			$(location).attr('href', '/');
+		}
+	});
 };
 
 function getUserID() {
-    $.ajax({
-        type:   'get',
-        url:    '/welcome/getUserID',
-        success:    function(result) {
-            $('#divUserID').text(result.id);
-        }
-    });
+	$.ajax({
+		type : 'get',
+		url : '/welcome/getUserID',
+		success : function(result) {
+			$('#divUserID').text(result.id);
+		}
+	});
 };
 
 function getTeamList() {
-    $.ajax({
-        type:   'get',
-        url:    '/welcome/getTeamList',
-        success:    function(teams) {
-        	console.log("getTeamList");
-        	if ( teams.length > 0 ) {
-	        	var li = [];
-	        	var i = 0;
-	        	
-	        	$.each(teams, function(k, v) {
-	        		li[i++] = '<li><a href="#" id="'+v.id+'"class="projectList">' + v.name+ '</a></li>';
-	        	});
-	        	$('#ulProjectList').empty();
-	        	$('#ulProjectList').append(li.join(''));
-			    $('.projectList').click(projectSelected);
-        	}
-        }
-    });
+	$.ajax({
+		type : 'get',
+		url : '/welcome/getTeamList',
+		success : function(teams) {
+			console.log("getTeamList");
+			if (teams.length > 0) {
+				var li = [];
+				var i = 0;
+
+				$.each(teams, function(k, v) {
+					li[i++] = '<li><a href="#" id="' + v.id + '"class="projectList">' + v.name + '</a></li>';
+				});
+				$('#ulProjectList').empty();
+				$('#ulProjectList').append(li.join(''));
+				$('.projectList').click(projectSelected);
+			}
+		}
+	});
 };
-function openDialog(){
+function openDialog() {
 	$('#dialogCreateTeam').modal({
-		backdrop: false,
-		keyboard: true
+		backdrop : false,
+		keyboard : true
 	});
 };
 
 function createTeamAction() {
-	
+
 	var json = {};
 	json["team_name"] = $('#inputTeamName').val();
-	
-	if ( json["team_name"] == "" ) {
+
+	if (json["team_name"] == "") {
 		alert("Insert Your Team Name");
 		return;
 	}
-	
+
 	$.ajax({
-		type: 'post',
-		url: '/welcome/createTeam',
-		data: json,
-		success: function() {
+		type : 'post',
+		url : '/welcome/createTeam',
+		data : json,
+		success : function() {
 			getTeamList();
 		}
 	});
-	
+
 	$('#dialogCreateTeam').modal('hide');
-	
+
 }
 
 function projectSelected() {
@@ -79,22 +79,14 @@ function projectSelected() {
 	var json = {};
 	json["project_name"] = $(this).text();
 	json["team_id"] = $(this).attr('id');
-	
+
 	$.ajax({
-		type: "post",
-		url: "/welcome/projectSelected",
+		type : "post",
+		url : "/welcome/projectSelected",
 		data : json,
-		success: function(result) {
+		success : function(result) {
 			$(location).attr('href', '/project');
 		}
 	});
 };
-
-
-
-
-
-
-
-
 
