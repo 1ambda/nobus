72acc6b18268093f3c751d951a9d849abfb673b6
unknown <john@.(none)>
2013-09-13 23:09:33 +0900

    d

---
+++ table.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE youth;
+
+USE youth
+
+CREATE TABLE user(
+	id CHAR(20) NOT NULL PRIMARY KEY,
+	pwd CHAR(20) NOT NULL,
+	last DATETIME);
+
+CREATE TABLE team(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name CHAR(20) NOT NULL);
+
+CREATE TABLE task(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	team_id INT NOT NULL,
+	name CHAR(30) NOT NULL,
+	start_date DATETIME,
+	due_date DATETIME,
+	finished TINYINT(1) DEFAULT 0,
+	FOREIGN KEY (team_id) REFERENCES team(id));
+
+ CREATE TABLE user_team(
+	user_id CHAR(20) NOT NULL,
+	team_id INT NOT NULL,
+	take_on TINYINT(1) DEFAULT 1,
+	PRIMARY KEY(user_id, team_id),
+	FOREIGN KEY (user_id) REFERENCES user(id),
+	FOREIGN KEY (team_id) REFERENCES team(id));
+	
+CREATE TABLE user_task(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	user_id CHAR(20) NOT NULL,
+	task_id INT NOT NULL,
+	task_on TINYINT(1) DEFAULT 1,
+	FOREIGN KEY (user_id) REFERENCES user(id),
+	FOREIGN KEY (task_id) REFERENCES task(id));
+
+CREATE TABLE comment(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	user_id char(20) NOT NULL,
+	task_id INT NOT NULL,
+	txt CHAR(200) NOT NULL,
+	comment_on TINYINT(1) DEFAULT 1,
+	FOREIGN KEY (user_id) REFERENCES user(id),
+	FOREIGN KEY (task_id) REFERENCES task(id));
