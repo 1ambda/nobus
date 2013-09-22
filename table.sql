e1cf83aa17b2daca3d33257ce64a4689cb8b2028
unknown <john@.(none)>
2013-09-22 20:58:32 +0900

    d

--- table.sql
+++ table.sql
@@ -47,3 +47,114 @@ CREATE TABLE comment(
 	comment_on TINYINT(1) DEFAULT 1,
 	FOREIGN KEY (user_id) REFERENCES user(id),
 	FOREIGN KEY (task_id) REFERENCES task(id));
+
+CREATE TABLE push(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	user_id CHAR(20) NOT NULL,
+	task_id INT NOT NULL,
+	txt TEXT,
+	start_date DATE NOT NULL,
+	due_date DATE NOT NULL,
+	checked TINYINT(1) NOT NULL DEFAULT 0,
+	FOREIGN KEY (user_id) REFERENCES user(id),
+	FOREIGN KEY (task_id) REFERENCES task(id)
+	);
+	
+CREATE TABLE toss(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	user_id CHAR(20) NOT NULL,
+	task_id INT NOT NULL,
+	txt TEXT,
+	start_date DATE NOT NULL,
+	due_date DATE NOT NULL,
+	checked TINYINT(1) NOT NULL DEFAULT 0,
+	FOREIGN KEY (user_id) REFERENCES user(id),
+	FOREIGN KEY (task_id) REFERENCES task(id)
+	);
+	
+CREATE TABLE submit(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	user_id CHAR(20) NOT NULL,
+	task_id INT NOT NULL,
+	txt TEXT,
+	submit_date DATE NOT NULL,
+	checked TINYINT(1) NOT NULL DEFAULT 0,
+	FOREIGN KEY (user_id) REFERENCES user(id),
+	FOREIGN KEY (task_id) REFERENCES task(id)
+	);
+	
+CREATE TABLE push_comment(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	push_id INT NOT NULL,
+	user_id CHAR(20) NOT NULL,
+	txt TEXT,
+	FOREIGN KEY (user_id) REFERENCES user(id),
+	FOREIGN KEY (push_id) REFERENCES push(id)
+	);
+	
+CREATE TABLE toss_comment(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	toss_id INT NOT NULL,
+	user_id CHAR(20) NOT NULL,
+	txt TEXT,
+	FOREIGN KEY (user_id) REFERENCES user(id),
+	FOREIGN KEY (toss_id) REFERENCES push(id)
+	);
+	
+CREATE TABLE submit_comment(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	submit_id INT NOT NULL,
+	user_id CHAR(20) NOT NULL,
+	txt TEXT,
+	FOREIGN KEY (user_id) REFERENCES user(id),
+	FOREIGN KEY (submit_id) REFERENCES push(id)
+	);
+
+CREATE TABLE push_file(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	push_id INT NOT NULL,
+	image CHAR(200),
+	file CHAR(200),
+	FOREIGN KEY (push_id) REFERENCES push(id)
+);
+
+CREATE TABLE toss_file(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	toss_id INT NOT NULL,
+	image CHAR(200),
+	file CHAR(200),
+	FOREIGN KEY (toss_id) REFERENCES toss(id)
+);
+
+CREATE TABLE submit_file(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	submit_id INT NOT NULL,
+	image CHAR(200),
+	file CHAR(200),
+	FOREIGN KEY (submit_id) REFERENCES submit(id)
+);
+
+CREATE TABLE push_comment_file(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	push_comment_id INT NOT NULL,
+	image CHAR(200),
+	file CHAR(200),
+	FOREIGN KEY (push_comment_id) REFERENCES push_comment(id)
+);
+
+CREATE TABLE toss_comment_file(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	toss_comment_id INT NOT NULL,
+	image CHAR(200),
+	file CHAR(200),
+	FOREIGN KEY (toss_comment_id) REFERENCES toss_comment(id)
+);
+
+CREATE TABLE submit_comment_file(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	submit_comment_id INT NOT NULL,
+	image CHAR(200),
+	file CHAR(200),
+	FOREIGN KEY (submit_comment_id) REFERENCES submit_comment(id)
+);
+
