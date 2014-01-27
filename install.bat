cc80a10ed94a1c8d79680c65ea9a7c95c9d6c4fc
Razenrote <ansterd@nate.com>
2014-01-28 00:19:15 +0900

    [Finished] package.json

--- install.bat
+++
@@ -1,7 +0,0 @@
-start /WAIT /b npm install mysql
-start /WAIT /b npm install express
-start /WAIT /b npm install generic-pool
-start /WAIT /b npm install ejs
-start /WAIT /b npm install socket.io
-start /WAIT /b npm install async
-
